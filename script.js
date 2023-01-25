b55d600702b6fb0112135b151443f677c8d111e4
chitranshxsharma <113257610+chitranshxsharma@users.noreply.github.com>
2023-01-25 22:47:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function myfun(){
+    document.getElementById("imgcenter").src = "pepsi001.png";
+    document.getElementById("main").style.backgroundColor = "#0261BF";
+}
+function myfun1(){
+    document.getElementById("imgcenter").src = "pepsi002.png";
+    document.getElementById("main").style.backgroundColor = "#e60c2c";
+}
+function myfun2(){
+    document.getElementById("imgcenter").src = "pepsi003.png";
+    document.getElementById("main").style.backgroundColor = "#1e1e1e";
+}
+
+
+
+    
